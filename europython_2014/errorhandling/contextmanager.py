d0bf6584ae1832b21bf47f26232e7a1f3260d3a6
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-07-21 14:54:50 +0200

    Changed example for context manager from a sockets to copying a file because as of Python 3 sockets already have a context manager.

--- europython_2014/errorhandling/contextmanager.py
+++ europython_2014/errorhandling/contextmanager.py
@@ -1,38 +1,115 @@
-'''
+"""
 Example context manager to be used together with the ``with`` statement.
-'''
-import socket
+"""
+import os
 
 
-class SocketClient():
-    '''Provide a ``socket`` and automatically close it when done.'''
-    def __init__(self, host, port):
-        self.socket = socket.create_connection((host, port))
+class CopyTask():
+    def __init__(self, source_path, target_path):
+        self.source_path = source_path
+        self.target_path = target_path
+        self._source_file = None
+        self._target_file = None
+
+        try:
+            print('  open source %s' % source_path)
+            self._source_file = open(source_path, 'rb')
+            print('  open target %s' % target_path)
+            self._target_file = open(target_path, 'wb')
+        except:
+            self.close()
+            raise
+
+    def run(self):
+        BUFFER_SIZE = 1024
+        buffer = self._source_file.read(BUFFER_SIZE)
+        while len(buffer) > 0:
+            self._target_file.write(buffer)
+            buffer = self._source_file.read(BUFFER_SIZE)
+
+    def close(self):
+        """Release resources acquired by `__init__()`."""
+        if self._target_file is not None:
+            print('  close target %s' % self.target_path)
+            self._target_file.close()
+            self._target_file = None
+        if self._source_file is not None:
+            print('  close source %s' % self.source_path)
+            self._source_file.close()
+            self._source_file = None
 
     def __enter__(self):
-        '''Value ``with`` statement should assign to its target variable.'''
+        """Value ``with`` statement should assign to its target variable."""
         return self
 
     def __exit__(self, type, value, traceback):
-        '''Release resources acquired by `__init__()`.'''
-        self.socket.shutdown(socket.SHUT_RDWR)
-        self.socket.close()
+        self.close()
+
+
+class NaiveCopyTask():
+    """
+    A naive variant for CopyTask for illustrative purpose that does not
+    cleanup properly if error happen during `__init__()`.
+    """
+    def __init__(self, source_path, target_path):
+        print('  open source %s' % source_path)
+        self._source_file = open(source_path, 'rb')
+        print('  open target %s' % target_path)
+        self._target_file = open(target_path, 'wb')
+
+    def run(self):
+        BUFFER_SIZE = 1024
+        buffer = self._source_file.read(BUFFER_SIZE)
+        while len(buffer) > 0:
+            self._target_file.write(buffer)
+            buffer = self._source_file.read(BUFFER_SIZE)
+
+    def close(self):
+        """Release resources acquired by `__init__()`."""
+        print('  close target %s' % self.target_path)
+        self._target_file.close()
+        print('  close source %s' % self.source_path)
+        self._source_file.close()
 
 
 if __name__ == '__main__':
-    # Example for a working connection.
-    with SocketClient('www.python.org', 80) as pythonOrg:
-        pythonOrg.socket.sendall(
-            'GET /index.html HTTP/1.0\n'
-            + 'Host: www.python.org\n'
-            + '\n')
-        reply = pythonOrg.socket.recv(64)
-    print('data obtained: %r...' % reply)
+    test_path = 'some.txt'
+    with open(test_path, 'w') as test_file:
+        for line_number in range(1, 10000):
+            test_file.write('this is line number %d' % line_number)
+            test_file.write(os.linesep)
 
-    # Example for a failed connection.
-    hostname = 'no.such.host'
+    print('example of a working CopyTask')
+    with CopyTask('some.txt', 'copy_of_some.txt') as some_copy_task:
+        some_copy_task.run()
+
+    print('example of a working CopyTask without using with')
+    another_copy_task = CopyTask('some.txt', 'copy_of_some.txt')
     try:
-        with SocketClient(hostname, 80) as pythonOrg:
-            assert False, 'connection must fail'
-    except socket.error as error:
-        print('cannot connect to %s: %s' % (hostname, error))
+        another_copy_task.run()
+    finally:
+        another_copy_task.close()
+
+    print('example of a broken CopyTask that cannot find the source')
+    source_path = 'no_such_file.txt'
+    target_path = 'copy_of_some.txt'
+    try:
+        with CopyTask(source_path, target_path) as broken_copy_task:
+            assert False, 'copy fail'
+    except IOError as error:
+        print('as expected: cannot copy %s to %s: %s' % (source_path, target_path, error))
+
+    print('example of a broken CopyTask that cannot open target but still closes the already opened source')
+    source_path = 'some.txt'
+    target_path = os.path.join('no_such_folder', 'copy_of_some.txt')
+    try:
+        with CopyTask(source_path, target_path) as broken_copy_task:
+            assert False, 'copy must fail'
+    except IOError as error:
+        print('as expected: cannot copy %s to %s: %s' % (source_path, target_path, error))
+
+    print('example for naive CopyTask (without proper cleanup in case of error')
+    source_path = 'some.txt'
+    target_path = 'copy_of_some.txt'
+    with CopyTask(source_path, target_path) as naive_copy_task:
+        naive_copy_task.run()
