12a84f8c9d88e9f13c565b0929e38cdd1503fad5
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-10-12 00:29:07 +0200

    Incorporated feedback from PyGRAZ: - Fixed slide on Unicode differences between Python versions. - Added example for named unicode literal. - Added example on how to continue encoding UTF-32 without BOM. - Added reference to six package for portable string types. - Improved terminology ("code points"). - Cleaned up slide on non issues (Nicht-Themen). - Fixed typos.

--- pygraz/unicode/examples.py
+++ pygraz/unicode/examples.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Examples and code excerpts for talk on "Unicode survival kit".
+Examples and code excerpts for talk on "Erste Hilfekasten für Unicode mit Python".
 """
 english_text = 'cheese spaetzle: 10 euro'
 print(english_text)
@@ -8,12 +8,11 @@ german_text = 'Käsespätzle: 10€'
 german_text = u'Käsespätzle: 10€'
 german_text = 'K\u00e4sesp\u00e4tzle: 10\u20ac'
 print(german_text)
-data = b'spae\x74\x7ale'
-print(data)
 
 _ENGLISCH_PANGRAM = 'the quick brown fox jumps over the lazy white dog'
 _GERMAN_PANGRAM = 'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg'
 _GERMAN_EURO_PANGRAM = 'Add'
+
 
 def hexdump_lines(text_bytes, break_after=16):
     hex_part = ''
@@ -34,10 +33,12 @@ def hexdump_lines(text_bytes, break_after=16):
     if hex_part != '':
         yield line_address, hex_part, text_part
 
+
 def print_hexdump(text_bytes):
     BREAK_AFTER = 16
     for address, hex_part, text_part in hexdump_lines(text_bytes, BREAK_AFTER):
         print('%08x: %-*s  |%s|' % (address, 3 * BREAK_AFTER, hex_part, text_part))
+
 
 for encoding in ('utf-32', 'utf-8', 'cp1252'):
     encoded_bytes = 'Spätzle: €10'.encode(encoding)
@@ -48,7 +49,7 @@ for encoding in ('utf-8', 'cp1252'):
     print(encoding)
     print_hexdump(_GERMAN_EURO_PANGRAM.encode(encoding))
 
-# Code works in Python 2.6+ and 3.2+.
+# Code works in Python 2.6+ and 3.3+.
 import io
 with io.open('menu.txt', 'w', encoding='utf-32') as menu_file:
     menu_file.write(u'Spätzle: 10€')
@@ -61,17 +62,26 @@ for c in ['ü', '€']:
     for encoding in ['utf-8', 'cp1252', 'iso-8859-15']:
         print('%s %s --> %r' % (c, encoding, c.encode(encoding)))
 
-#import sys
-#print(sys.getdefaultencoding())
+# Show the (basically random) default encoding.
+import sys
+print(sys.getdefaultencoding())
 
+# The euro sign in its various incarnations.
 euro_sign = '€'
 euro_sign = '\u20ac'
 euro_sign = chr(0x20ac)  # Python 2: unichr(0x20a0)
 euro_sign = chr(2 * 16**3 + 0 * 16**2 + 10 * 16**1 + 12 * 16**0)
 euro_sign = chr(8364)
+euro_sign = '\N{EURO SIGN}'
+import unicodedata
+euro_sign = unicodedata.lookup('EURO SIGN')
 print(euro_sign)
-print(hex(ord('樹')))
 
+print(hex(ord('樹')))
+print('\u20ac'.encode('utf-8'))  # euro sign as UTF-8
+_= b'\xe2\x82\xac'
+
+# Example stack traces for UnicodeError.
 import sys
 import traceback
 try:
@@ -86,6 +96,7 @@ try:
 except UnicodeDecodeError as error:
     traceback.print_exc(file=sys.stdout)
 
+# Hack cp1252 to map every byte, even undefined ones like b'\x81'.
 import codecs
 from encodings import cp1252
 
