c6314615b72daf175ec1ed86e5e87b6c12d8eb68
Thomas Aglassinger <roskakori@users.sourceforge.net>
2015-04-24 21:48:36 +0200

    Fixed indentation.

--- pygraz/opensourceprojects/test_dividemo.py
+++ pygraz/opensourceprojects/test_dividemo.py
@@ -1,16 +1,16 @@
-    """
-    Tests for dividemo.
-    """
-    import unittest
+"""
+Tests for dividemo.
+"""
+import unittest
 
-    from dividemo import dividemo
+from dividemo import dividemo
 
-    class DividemoTest(unittest.TestCase):
-        def test_can_divide(self):
-            self.assertEqual(2, dividemo.divided(10, 5))
+class DividemoTest(unittest.TestCase):
+    def test_can_divide(self):
+        self.assertEqual(2, dividemo.divided(10, 5))
 
-        def test_can_print_divided(self):
-            dividemo.main(['10', '5'])
+    def test_can_print_divided(self):
+        dividemo.main(['10', '5'])
 
-        def test_fails_on_non_integer_divisor(self):
-            self.assertRaises(SystemExit, dividemo.main, ['10', 'hello'])
+    def test_fails_on_non_integer_divisor(self):
+        self.assertRaises(SystemExit, dividemo.main, ['10', 'hello'])
