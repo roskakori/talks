be11830ec16998289432b75e73c37d2260619983
Thomas Aglassinger <roskakori@users.sourceforge.net>
2019-02-27 10:50:05 +0100

    Added fragments on snetiment analysis and semantic search.

---
+++ linuxtage/Introduction to natural language processing/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Setup Python environment
+set -e
+set -x
+
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
---
+++ linuxtage/Introduction to natural language processing/start_notebook.sh
@@ -0,0 +1,3 @@
+set -e
+source venv/bin/activate
+python3 -m jupyter notebook
